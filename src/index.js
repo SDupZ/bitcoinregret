7dd3d384503b416f2cb921cf463872b268214e6f
Simon du Preez <s.dupreez@crimsoneducation.org>
2017-10-22 16:56:26 +1300

    Fixed site verification

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import './index.css';
 import App from './components/unique/App';
-import SiteVerification from './components/unique/SiteVerification'
 import store from './store'
 
 import ReactGA from 'react-ga';
@@ -13,7 +12,6 @@ ReactGA.initialize('UA-70937828-2');
 const AppWithRoutes = () => (
     <Router>
         <Switch>
-            <Route exact path="/googlee75a16594c6a3a19.html" component={SiteVerification} />
             <Route path="/" component={App} />
         </Switch>
     </Router>
