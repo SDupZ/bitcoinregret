1e6b7f671f53e47ca5ca1be1c73e18064bc11109
Simon du Preez <s.dupreez@crimsoneducation.org>
2017-10-22 16:38:15 +1300

    Added google site verification. Added react router

---
+++ src/components/unique/SiteVerification/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function SiteVerification() {
+  return (
+    <body>
+      google-site-verification: googlee75a16594c6a3a19.html
+    </body>
+  );
+};
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import './index.css';
 import App from './components/unique/App';
+import SiteVerification from './components/unique/SiteVerification'
 import store from './store'
 
 import ReactGA from 'react-ga';
 ReactGA.initialize('UA-70937828-2');
+
+const AppWithRoutes = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/googlee75a16594c6a3a19.html" component={SiteVerification} />
+            <Route path="/" component={App} />
+        </Switch>
+    </Router>
+)
 
 const logPageView = () => {
     ReactGA.set({page: window.location.pathname + window.location.search });
@@ -16,7 +27,7 @@ const logPageView = () => {
 
 ReactDOM.render(
     <Provider store={store}>
-        {logPageView() && <App />}
+        {logPageView() && <AppWithRoutes />}
     </Provider>,
     document.getElementById('root'));
 
