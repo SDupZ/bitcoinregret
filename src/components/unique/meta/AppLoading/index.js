2c3fd8226e3b1f80ad7907e2044e997b8134c09a
Simon du Preez <s.dupreez@crimsoneducation.org>
2017-12-12 22:29:21 +1300

    Removed unused component

--- src/components/unique/meta/AppLoading/index.js
+++
@@ -1,9 +0,0 @@
-import {connect} from 'react-redux'
-import AppLoading from './AppLoading'
-
-const mapStateToProps = (state) => ({
-  loading: state.meta.loading,
-})
-
-
-export default connect(mapStateToProps)(AppLoading)
