2149c4d69f7a016b146bbaf2da6cd0c6d3298915
Simon du Preez <s.dupreez@crimsoneducation.org>
2019-06-08 16:08:05 +1200

    Cleanup

--- src/components/archive/generic/InputField/index.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import InputField from './InputField';
-
-const mapStateToProps = (state, ownProps) => ({
-  value: ownProps.value(state)
-});
-
-export default connect(mapStateToProps)(InputField);
--- src/components/archive/molecules/LoadingBar/index.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import LoadingBar from './LoadingBar'
-
-const mapStateToProps = (state) => ({
-  isPageLoading: state.meta.loading,
-})
-
-export default connect(mapStateToProps)(LoadingBar)
